8ce3545a27932a172363d3d74136524c43fe3faa
cetem99 <lucas.vianna99@gmail.com>
2025-07-30 15:37:15 -0300

    Implementaçao do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
- selector: 'app-root',
+  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   template: `
-    <router-outlet></router-outlet>
+    <div class="content-wrapper">
+      <router-outlet></router-outlet>
+    </div>
+    <app-footer></app-footer>
   `,
-  styleUrls: ['./app.component.scss']
+  styles: [`
+    :host {
+      display: block;
+      position: relative;
+      min-height: 100vh;
+    }
+    
+    .content-wrapper {
+      padding-bottom: 80px; /* Altura do footer + um pouco extra */
+      margin-bottom: 80px;
+    }
+    
+    app-footer {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      width: 100%;
+      height: 60px; /* Altura fixa do footer */
+      z-index: 1000;
+    }
+  `]
 })
 export class AppComponent {
   title = 'pessoas-app';
